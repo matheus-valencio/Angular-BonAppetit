ed3618793bada4f0cd9f811e860aa4a1642f1f53
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-06-09 02:28:02 -0300

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CardapioComponent } from './cardapio/cardapio.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, RouterModule.forRoot([
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'cardapio', component: CardapioComponent}
   ])],
 
   providers: [],
