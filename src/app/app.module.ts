d3f7e7aec260df6f31918fe0c5756c9e3111316e
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-06-09 02:02:32 -0300

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot([])],
+  imports: [BrowserModule, RouterModule.forRoot([
+    {path: '', component: HomeComponent}
+  ])],
 
   providers: [],
   bootstrap: [AppComponent],
