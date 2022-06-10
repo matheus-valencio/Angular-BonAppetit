1dfcfd0faf1014d1d4d4bf7b7f95b9420245314e
isammt <bella.trigo14@gmail.com>
2022-06-10 20:43:09 -0300

    2 files modified

--- src/app/pedido/pedido.component.ts
+++ src/app/pedido/pedido.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { TasksService } from '../tasks.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pedido',
   templateUrl: './pedido.component.html',
   styleUrls: ['./pedido.component.css']
 })
-export class PedidoComponent implements OnInit {
- 
-  constructor(public tasks: TasksService) {}
-  
-  ngOnInit() {
-  }
+export class PedidoComponent implements OnInit {item;
+  constructor(public tasks: TasksService,
+    private route: ActivatedRoute
+    ) {}
 
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      this.item = this.tasks.getList()[params.get('index')];
+    })
+  }
 }
