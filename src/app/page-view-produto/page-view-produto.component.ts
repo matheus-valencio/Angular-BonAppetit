6b4cb66724359e29cd8260433b827f82f1757f94
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-06-09 23:45:04 -0300

    2 files modified, 2 added

---
+++ src/app/page-view-produto/page-view-produto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-view-produto',
+  templateUrl: './page-view-produto.component.html',
+  styleUrls: ['./page-view-produto.component.css']
+})
+export class PageViewProdutoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pedido/pedido.component.ts
+++ src/app/pedido/pedido.component.ts
@@ -6,9 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pedido.component.css']
 })
 export class PedidoComponent implements OnInit {
+  id: number;
+  nome_sabor: string;
+  descricao: string
 
-  constructor() { }
-
+  constructor() {
+    this.id = 1;
+    this.nome_sabor = 'mussarela';
+    this.descricao = 'Pizza com queijo mussarela'
+   }
+  
   ngOnInit() {
   }
 
