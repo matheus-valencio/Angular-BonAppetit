8c34093747df06a27eb43c111296814b42c9b18f
isammt <bella.trigo14@gmail.com>
2022-06-10 12:25:53 -0300

    2 files modified

--- src/app/admin.service.ts
+++ src/app/admin.service.ts
@@ -11,7 +11,31 @@ categoria_pk_produto: string;
 }
 @Injectable()
 export class AdminService {
-
+lista: Array<Admin> = [];
   constructor() { }
 
+  getLista(){
+    return this.lista; 
+  }
+
+  adicionarLista(
+    pk_produto: number,
+    nm_produto: string,
+    ds_produto: string,
+    nm_imagem: string,
+    vl_produto: number,
+    categoria_pk_produto: string,
+  )
+  {
+    this.lista.push({pk_produto,
+      nm_produto,
+      ds_produto,
+      nm_imagem,
+      vl_produto,
+      categoria_pk_produto})
+  }
+
+  deleteItem(index) {
+    this.lista.splice(index, 1);
+  }
 }
